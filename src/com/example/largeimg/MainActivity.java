3db865d0ec299e857770cf3f96dcec10ee54011f
LYL <liyulin05@126.com>
2015-12-25 16:56:47 +0800

    first

---
+++ src/com/example/largeimg/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.largeimg;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * 
+ * @author steve
+ *
+ */
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
